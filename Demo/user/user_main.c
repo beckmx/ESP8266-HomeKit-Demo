398730606d46c52c5841748bb4a8d695b258875f
Development Team <andres.santos@consultware.mx>
2017-09-07 11:12:28 -0500

    commented homekit, activated file

--- Demo/user/user_main.c
+++ Demo/user/user_main.c
@@ -59,6 +59,13 @@
 #include "hkc.h"
 #include "gpio.h"
 #include "queue.h"
+#include <stdio.h>
+#include <fatfs/ff.h>
+
+#define TEST_FILENAME "/test_loooong_filename.txt"
+#define TEST_CONTENTS "Hello! It's FatFs on esp8266 with ESP Open RTOS!"
+#define READBUF_SIZE 256
+#define DELAY_MS 3000
 
 xQueueHandle identifyQueue;
 
@@ -66,6 +73,106 @@ struct  gpio {
     int aid;
     int iid;
 } gpio2;
+
+static const char contents[] = TEST_CONTENTS;
+
+static const char *results[] = {
+    [FR_OK]                  = "Succeeded",
+    [FR_DISK_ERR]            = "A hard error occurred in the low level disk I/O layer",
+    [FR_INT_ERR]             = "Assertion failed",
+    [FR_NOT_READY]           = "The physical drive cannot work",
+    [FR_NO_FILE]             = "Could not find the file",
+    [FR_NO_PATH]             = "Could not find the path",
+    [FR_INVALID_NAME]        = "The path name format is invalid",
+    [FR_DENIED]              = "Access denied due to prohibited access or directory full",
+    [FR_EXIST]               = "Access denied due to prohibited access",
+    [FR_INVALID_OBJECT]      = "The file/directory object is invalid",
+    [FR_WRITE_PROTECTED]     = "The physical drive is write protected",
+    [FR_INVALID_DRIVE]       = "The logical drive number is invalid",
+    [FR_NOT_ENABLED]         = "The volume has no work area",
+    [FR_NO_FILESYSTEM]       = "There is no valid FAT volume",
+    [FR_MKFS_ABORTED]        = "The f_mkfs() aborted due to any problem",
+    [FR_TIMEOUT]             = "Could not get a grant to access the volume within defined period",
+    [FR_LOCKED]              = "The operation is rejected according to the file sharing policy",
+    [FR_NOT_ENOUGH_CORE]     = "LFN working buffer could not be allocated",
+    [FR_TOO_MANY_OPEN_FILES] = "Number of open files > _FS_LOCK",
+    [FR_INVALID_PARAMETER]   = "Given parameter is invalid"
+};
+static char readbuf[READBUF_SIZE];
+
+static bool failed(FRESULT res)
+{
+    bool fail = res != FR_OK;
+    if (fail)
+        os_printf("\n  Error: ");
+        
+    os_printf("\n  %s\n", results[res]);
+    return fail;
+}
+
+void check_fatfs()
+{
+    const char *vol = f_gpio_to_volume(CS_GPIO_PIN);
+
+    os_printf("\nCreating test file\n----------------------------\n");
+
+    FATFS fs;
+    // Mount filesystem
+    os_printf("f_mount(&fs, \"%s\")", vol);
+    if (failed(f_mount(&fs, vol, 1)))
+        return;
+
+    // Set default drive
+    os_printf("f_chdrive(\"%s\")", vol);
+    if (failed(f_chdrive(vol)))
+        return;
+
+    FIL f;
+    // Create test file
+    os_printf("f_open(&f, \"%s\", FA_WRITE | FA_CREATE_ALWAYS)", TEST_FILENAME);
+    if (failed(f_open(&f, TEST_FILENAME, FA_WRITE | FA_CREATE_ALWAYS)))
+        return;
+
+    size_t written;
+    // Write test string
+    os_printf("f_write(&f, \"%s\")", contents);
+    if (failed(f_write(&f, contents, sizeof(contents) - 1, &written)))
+        return;
+    os_printf("  Bytes written: %d\n", written);
+
+    // Close file
+    os_printf("f_close(&f)");
+    if (failed(f_close(&f)))
+        return;
+
+    os_printf("\nReading test file\n----------------------------\n");
+
+    // Open test file
+    os_printf("f_open(&f, \"%s\", FA_READ)", TEST_FILENAME);
+    if (failed(f_open(&f, TEST_FILENAME, FA_READ)))
+        return;
+
+    os_printf("  File size: %u\n", (uint32_t)f_size(&f));
+
+    size_t readed;
+    // Read file
+    os_printf("f_read(&f, ...)");
+    if (failed(f_read(&f, readbuf, sizeof(readbuf) - 1, &readed)))
+        return;
+    readbuf[readed] = 0;
+
+    os_printf("  Readed %u bytes, test file contents: %s\n", readed, readbuf);
+
+    // Close file
+    os_printf("f_close(&f)");
+    if (failed(f_close(&f)))
+        return;
+
+    // Unmount
+    os_printf("f_mount(NULL, \"%s\")", vol);
+    if (failed(f_mount(NULL, vol, 0)))
+        return;
+}
 
 void    led_task(void *arg) //make transfer of gpio via arg, starting as a static variable in led routine
 {
@@ -207,7 +314,9 @@ void user_init(void)
     
     //try to only do the bare minimum here and do the rest in hkc_user_init
     // if not you could easily run out of stack space during pairing-setup
-    hkc_init("HomeACcessory");
+    //hkc_init("HomeACcessory");
+    check_fatfs();
+    printf("\n\n");
     
     os_printf("end of user_init @ %d\n",system_get_time()/1000);
 }
