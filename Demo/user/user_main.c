d087e4dc314215ad7eb28294d4287aea091bee1a
Development Team <andres.santos@consultware.mx>
2017-09-07 17:15:48 -0500

    cambio de filesystem

--- Demo/user/user_main.c
+++ Demo/user/user_main.c
@@ -60,6 +60,10 @@
 #include "gpio.h"
 #include "queue.h"
 
+#include "spiffs.h"
+#include <stdlib.h>
+#include "spiffs_test_params.h"
+
 xQueueHandle identifyQueue;
 
 struct  gpio {
@@ -84,6 +88,100 @@ void    led_task(void *arg) //make transfer of gpio via arg, starting as a stati
         send_events(NULL,gpio2.aid,gpio2.iid);
     }
 }
+
+
+
+static void example_read_file_posix()
+{
+    const int buf_size = 0xFF;
+    uint8_t buf[buf_size];
+
+    int fd = open("test.txt", O_RDONLY);
+    if (fd < 0) {
+        printf("Error opening file\n");
+        return;
+    }
+
+    int read_bytes = read(fd, buf, buf_size);
+    printf("Read %d bytes\n", read_bytes);
+
+    buf[read_bytes] = '\0';    // zero terminate string
+    printf("Data: %s\n", buf);
+
+    close(fd);
+}
+
+static void example_read_file_spiffs()
+{
+    const int buf_size = 0xFF;
+    uint8_t buf[buf_size];
+
+    spiffs_file fd = SPIFFS_open(&fs, "other.txt", SPIFFS_RDONLY, 0);
+    if (fd < 0) {
+        printf("Error opening file\n");
+        return;
+    }
+
+    int read_bytes = SPIFFS_read(&fs, fd, buf, buf_size);
+    printf("Read %d bytes\n", read_bytes);
+
+    buf[read_bytes] = '\0';    // zero terminate string
+    printf("Data: %s\n", buf);
+
+    SPIFFS_close(&fs, fd);
+}
+
+static void example_write_file()
+{
+    uint8_t buf[] = "Example data, written by ESP8266";
+
+    int fd = open("other.txt", O_WRONLY|O_CREAT, 0);
+    if (fd < 0) {
+        printf("Error opening file\n");
+        return;
+    }
+
+    int written = write(fd, buf, sizeof(buf));
+    printf("Written %d bytes\n", written);
+
+    close(fd);
+}
+
+static void example_fs_info()
+{
+    uint32_t total, used;
+    SPIFFS_info(&fs, &total, &used);
+    printf("Total: %d bytes, used: %d bytes", total, used);
+}
+
+void test_task(void *pvParameters)
+{
+#if SPIFFS_SINGLETON == 1
+    esp_spiffs_init();
+#else
+    // for run-time configuration when SPIFFS_SINGLETON = 0
+    esp_spiffs_init(0x200000, 0x10000);
+#endif
+
+    if (esp_spiffs_mount() != SPIFFS_OK) {
+        printf("Error mount SPIFFS\n");
+    }
+
+    while (1) {
+        vTaskDelay(2000 / portTICK_PERIOD_MS);
+
+        example_write_file();
+
+        example_read_file_posix();
+
+        example_read_file_spiffs();
+
+        example_fs_info();
+
+        printf("\n\n");
+    }
+}
+
 
 void led(int aid, int iid, cJSON *value, int mode)
 {
@@ -226,7 +324,7 @@ void user_init(void)
     //try to only do the bare minimum here and do the rest in hkc_user_init
     // if not you could easily run out of stack space during pairing-setup
     //hkc_init("HomeACcessory");
-    vSampleFunction();
+    xTaskCreate(test_task, "test_task", 1024, NULL, 2, NULL);
     os_printf("end of user_init @ %d\n",system_get_time()/1000);
 }
 
