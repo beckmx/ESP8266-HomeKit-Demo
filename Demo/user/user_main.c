a0c898ccc451f99a4e7b659551802a9076f9e672
Development Team <andres.santos@consultware.mx>
2017-09-12 12:17:12 -0500

    changes to self server

--- Demo/user/user_main.c
+++ Demo/user/user_main.c
@@ -77,6 +77,15 @@
 #include <sys/stat.h>
 
 
+#include "espconn.h"
+#include "soft_ap.h"
+
+#define DEMO_AP_SSID      "WT_TEST"
+#define DEMO_AP_PASSWORD  "1234567abc"
+#define SOFT_AP_SSID      "DEMO_AP"
+#define SOFT_AP_PASSWORD  "12345678"
+
+
 xQueueHandle identifyQueue;
 extern spiffs workingFS;
 struct esp_spiffs_config config;
@@ -234,8 +243,9 @@ static void example_read_file_spiffs()
     
     char out[20] = {0};
     int pfd = open("myfile",O_RDWR);
-    if (read(pfd, out, 20) < 0)
-         printf("read errno \n");
+    if (read(pfd, out, 20) < 0){
+        printf("read errno \n");
+    }   
     close(pfd);
     printf("--> %s <--\n", out);
 }
@@ -256,12 +266,31 @@ static void example_write_file()
     close(pfd);
 }
 
-static void example_fs_info()
+void httpd_task(void *pvParameters)
 {
-    uint32_t total, used;
-    SPIFFS_info(&workingFS, &total, &used);
-    os_printf("Total: %d bytes, used: %d bytes", total, used);
+    tCGI pCGIs[] = {
+        {"/gpio", (tCGIHandler) gpio_cgi_handler},
+        {"/about", (tCGIHandler) about_cgi_handler},
+        {"/websockets", (tCGIHandler) websocket_cgi_handler},
+    };
+
+    const char *pcConfigSSITags[] = {
+        "uptime", // SSI_UPTIME
+        "heap",   // SSI_FREE_HEAP
+        "led"     // SSI_LED_STATE
+    };
+
+    /* register handlers and start the server */
+    http_set_cgi_handlers(pCGIs, sizeof (pCGIs) / sizeof (pCGIs[0]));
+    http_set_ssi_handler((tSSIHandler) ssi_handler, pcConfigSSITags,
+            sizeof (pcConfigSSITags) / sizeof (pcConfigSSITags[0]));
+    websocket_register_callbacks((tWsOpenHandler) websocket_open_cb,
+            (tWsHandler) websocket_cb);
+    httpd_init();
+
+    for (;;);
 }
+
 
 void test_task(void *pvParameters)
 {
@@ -288,28 +317,89 @@ void test_task(void *pvParameters)
 
         example_read_file_spiffs();
 
-        example_fs_info();
+        
 
         os_printf("\n\n");
     }
 }
 
-void flash_test() {
-    os_printf("SDK version:%s\n", system_get_sdk_version());
- 
-     uint32 temp[4]={122323, 13, 14, 15};
-     uint32 temp1[4]={0};
- 
-     int i = spi_flash_erase_sector(0x8c);
-     os_printf("spi_flash_erase_sector: %d\n", i);
-     int o = spi_flash_write(0x8c000, temp, sizeof(temp));
-     os_printf("spi_flash_write: %d\n", o);
- 
-     int p = spi_flash_read(0x8c000, temp1, sizeof(temp1));
-     os_printf("spi_flash_read: %d\n", p);
- 
-     os_printf("read :%x, %x, %x, %x\n", temp1[0], temp1[1], temp1[2], temp1[3]);
- }
+void wifi_handle_event_cb(System_Event_t *evt)
+{
+    printf("event %x\n", evt->event_id);
+
+    switch (evt->event_id) {
+        case EVENT_STAMODE_CONNECTED:
+            printf("connect to ssid %s, channel %d\n", evt->event_info.connected.ssid,
+                    evt->event_info.connected.channel);
+            break;
+        case EVENT_STAMODE_DISCONNECTED:
+            printf("disconnect from ssid %s, reason %d\n", evt->event_info.disconnected.ssid,
+                    evt->event_info.disconnected.reason);
+            break;
+        case EVENT_STAMODE_AUTHMODE_CHANGE:
+            printf("mode: %d -> %d\n", evt->event_info.auth_change.old_mode, evt->event_info.auth_change.new_mode);
+            break;
+        case EVENT_STAMODE_GOT_IP:
+            printf("ip:" IPSTR ",mask:" IPSTR ",gw:" IPSTR, IP2STR(&evt->event_info.got_ip.ip),
+                    IP2STR(&evt->event_info.got_ip.mask), IP2STR(&evt->event_info.got_ip.gw));
+            printf("\n");
+            break;
+        case EVENT_SOFTAPMODE_STACONNECTED:
+            printf("station: " MACSTR "join, AID = %d\n", MAC2STR(evt->event_info.sta_connected.mac),
+                    evt->event_info.sta_connected.aid);
+            break;
+        case EVENT_SOFTAPMODE_STADISCONNECTED:
+            printf("station: " MACSTR "leave, AID = %d\n", MAC2STR(evt->event_info.sta_disconnected.mac),
+                    evt->event_info.sta_disconnected.aid);
+            break;
+        default:
+            break;
+    }
+} 
+
+void conn_ap_init(void)
+{
+	wifi_set_opmode(STATIONAP_MODE);
+	struct station_config config;
+	memset(&config,0,sizeof(config));  //set value of config from address of &config to width of size to be value '0'
+	sprintf(config.ssid, DEMO_AP_SSID);
+	sprintf(config.password, DEMO_AP_PASSWORD);
+	wifi_station_set_config(&config);         
+	wifi_set_event_handler_cb(wifi_handle_event_cb);
+	wifi_station_connect();
+}
+
+void soft_ap_init(void)
+{
+    wifi_set_opmode(SOFTAP_MODE);
+    struct softap_config *config = (struct softap_config *) zalloc(sizeof(struct softap_config)); // initialization
+    wifi_softap_get_config(config); // Get soft-AP config first.
+    sprintf(config->ssid, SOFT_AP_SSID);
+    sprintf(config->password, SOFT_AP_PASSWORD);
+    config->authmode = AUTH_WPA_WPA2_PSK;
+    config->ssid_len = 0; // or its actual SSID length
+    config->max_connection = 4;
+    wifi_softap_set_config(config); // Set ESP8266 soft-AP config
+    free(config);
+    struct station_info * station = wifi_softap_get_station_info();
+    while (station) {
+        printf("bssid : MACSTR, ip : IPSTR/n", MAC2STR(station->bssid), IP2STR(&station->ip));
+        station = STAILQ_NEXT(station, next);
+    }
+    wifi_softap_free_station_info(); // Free it by calling functionss
+    wifi_softap_dhcps_stop(); // disable soft-AP DHCP server
+    struct ip_info info;
+    IP4_ADDR(&info.ip, 192, 168, 5, 1); // set IP
+    IP4_ADDR(&info.gw, 192, 168, 5, 1); // set gateway
+    IP4_ADDR(&info.netmask, 255, 255, 255, 0); // set netmask
+    wifi_set_ip_info(SOFTAP_IF, &info);
+    struct dhcps_lease dhcp_lease;
+    IP4_ADDR(&dhcp_lease.start_ip, 192, 168, 5, 100);
+    IP4_ADDR(&dhcp_lease.end_ip, 192, 168, 5, 105);
+    wifi_softap_set_dhcps_lease(&dhcp_lease);
+    wifi_softap_dhcps_start(); // enable soft-AP DHCP server
+}
+
 /******************************************************************************
  * FunctionName : user_init
  * Description  : entry of user application, init user function here
@@ -332,8 +422,9 @@ void user_init(void)
     //try to only do the bare minimum here and do the rest in hkc_user_init
     // if not you could easily run out of stack space during pairing-setup
     //hkc_init("HomeACcessory");
-    xTaskCreate(test_task, "test_task", 1024, NULL, 2, NULL);
+    //xTaskCreate(test_task, "test_task", 1024, NULL, 2, NULL);
     //flash_test();
+    soft_ap_init();
     os_printf("end of user_init @ %d\n",system_get_time()/1000);
 }
 
