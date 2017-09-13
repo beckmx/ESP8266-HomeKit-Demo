5c55bd0af9c8746c1ed3c740b57f03ae59c19123
Development Team <andres.santos@consultware.mx>
2017-09-13 15:16:22 -0500

    lala

--- Demo/user/user_main.c
+++ Demo/user/user_main.c
@@ -392,8 +392,10 @@ void httpd_task(void *pvParameters)
                         array[i++] = p;
                         p = strtok (NULL, "\r");
                     }
-                    os_printf("uri_post: %s", array[14]);
-                    os_printf("uri_post: %s", array[15]);
+                    os_printf("uri_post12: %s", array[12]);
+                    os_printf("uri_post13: %s", array[13]);
+                    os_printf("uri_post14: %s", array[14]);
+                    os_printf("uri_post15: %s", array[15]);
                     // if (!strncmp(uri, "/on", max_uri_len))
                     //     // gpio_write(2, false);
                     //     os_printf("should turn ON led");
