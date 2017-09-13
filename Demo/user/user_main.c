24c78519f9fb093ea33e24350f6dda8f69d1d671
Development Team <andres.santos@consultware.mx>
2017-09-12 22:00:09 -0500

    cambios

--- Demo/user/user_main.c
+++ Demo/user/user_main.c
@@ -384,7 +384,7 @@ void httpd_task(void *pvParameters)
                     char *sp1, *sp2;
                     /* extract URI */
                     sp1 = (char*)data + 2048;
-                    char ch = "fname";
+                    char ch = {"fname"};
                     sp2 = memchr(sp1, ch, max_uri_len);
                     int len = sp2 - sp1;
                     memcpy(uri, sp1, len);
