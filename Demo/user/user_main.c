d4ff64bab892db8904ab35e628c96b7e5881b553
Development Team <andres.santos@consultware.mx>
2017-12-08 14:29:23 -0600

    trying to make work 512k

--- Demo/user/user_main.c
+++ Demo/user/user_main.c
@@ -266,7 +266,7 @@ void mount_filesystem()
         config.phys_size = FS3_FLASH_SIZE;
         config.phys_addr = FS3_FLASH_ADDR;
         config.phys_erase_block = SECTOR_SIZE2;
-        config.log_block_size = LOG_BLOCK;
+        config.log_block_size = LOG_BLOCK2;
         config.log_page_size = LOG_PAGE;
         config.fd_buf_size = FD_BUF_SIZE * 2;
         config.cache_buf_size = CACHE_BUF_SIZE;
