5ee66635e21c77e95a75cc281b0059d6df973398
Development Team <andres.santos@consultware.mx>
2017-12-07 23:12:42 -0600

    try to compile against 512kb

--- Demo/include/spiffs_test_params.h
+++ Demo/include/spiffs_test_params.h
@@ -8,14 +8,11 @@
  
  #define FS1_FLASH_SIZE      (128*1024)
  #define FS2_FLASH_SIZE      (128*1024)
- #define FS3_FLASH_SIZE      (128*512)
  
  #define FS1_FLASH_ADDR      (1024*1024)
  #define FS2_FLASH_ADDR      (1280*1024)
- #define FS3_FLASH_ADDR      (1024*512)
  
  #define SECTOR_SIZE         (4*1024) 
- #define SECTOR_SIZE2         (512) 
  #define LOG_BLOCK           (SECTOR_SIZE)
  #define LOG_PAGE            (128)
  
