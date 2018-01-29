f1cc996d08d2acd6608f2c3ed0cb84d5bdc46558
Andres Santos <andressantosbecerra@andruino.local>
2018-01-29 00:45:03 -0600

    getting saved ssid

--- Demo/user/user_main.c
+++ Demo/user/user_main.c
@@ -587,7 +587,7 @@ void user_init(void)
     uint8_t hwaddr[6];
     static char my_id[32];
     wifi_get_macaddr(STATION_IF, (uint8_t*)hwaddr);
-    os_printf("CURRENT_MAC:%s\n", hwaddr);
+    os_printf("CURRENT_MAC:%s\n", String((char *)hwaddr[5]));
     //snprintf(my_id, sizeof(my_id), "%02x%02x%02x%02x%02x%02x", MAC2STR(hwaddr));
     
     //mount_filesystem();
